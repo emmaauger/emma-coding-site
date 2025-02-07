a04d0cff56c9e45ac92461fb5635aedf4cd2e80b
emmaauger <83782933+emmaauger@users.noreply.github.com>
2025-02-07 13:53:20 -0500

    fixed dependency, starting design

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Explore the Brain of Emma Auger Hirner</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          All code for this project can be found at &nbsp
+          <a
+            className="App-link"
+            href="https://github.com/emmaauger"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
